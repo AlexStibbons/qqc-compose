1de93a0332c7beb999adae20195a4ea36e3a8bf3
AlexStibbons <pantic.aleks@gmail.com>
2023-12-14 14:14:57 +0100

    resolve dependency issues

--- app/src/main/java/com/stibbons/qqc_compose/QqcApplication.kt
+++ app/src/main/java/com/stibbons/qqc_compose/QqcApplication.kt
@@ -4,21 +4,23 @@ import android.app.Application
 import com.stibbons.qqc_compose.data.RepoImpl
 import com.stibbons.qqc_compose.data.Repository
 import com.stibbons.qqc_compose.data.SomeService
-import com.stibbons.qqc_compose.domain.FetchUseCase
+import com.stibbons.qqc_compose.domain.FetchData
+import org.koin.core.context.GlobalContext.startKoin
+import org.koin.dsl.module
 
 class QqcApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        /*startKoin{
+        startKoin{
             modules(appModule)
-        }*/
+        }
     }
 }
 
-/*
+
 val appModule = module {
     single { SomeService() }
     single<Repository> { RepoImpl(get()) }
-    factory { FetchUseCase(get()) }
-}*/
+    factory { FetchData(get()) }
+}
--- app/src/main/java/com/stibbons/qqc_compose/domain/BaseUseCase.kt
+++ app/src/main/java/com/stibbons/qqc_compose/domain/BaseUseCase.kt
@@ -2,8 +2,6 @@ package com.stibbons.qqc_compose.domain
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-
-
 abstract class UseCase<InputType, ReturnType>(
    protected val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) where InputType : Any? {
