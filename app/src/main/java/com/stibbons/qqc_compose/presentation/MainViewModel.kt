4ca4f38057fffb1e47a8ae36a4bc88cffc56d34a
AlexStibbons <pantic.aleks@gmail.com>
2023-12-18 17:18:58 +0100

    minor cleanup

--- app/src/main/java/com/stibbons/qqc_compose/presentation/MainViewModel.kt
+++ app/src/main/java/com/stibbons/qqc_compose/presentation/MainViewModel.kt
@@ -5,32 +5,29 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.stibbons.qqc_compose.domain.FetchData
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 internal class MainViewModel(
     private val fetchData: FetchData
 ): ViewModel() {
 
-    private var _allMessages = mutableListOf<MsgItemPresentation>()
-    val allMessages : List<MsgItemPresentation> get() = _allMessages
-
     private var _state = MutableLiveData<ViewState>()
     val screenState: LiveData<ViewState> get() = _state
 
     fun fetchData() = viewModelScope.launch {
-        _allMessages.clear()
         fetchData.run()
             .onEach { result ->
                 result.toPresentation().let {
                     _state.value = ViewState.Item(it)
-                    _allMessages.add(it)
                 }
             }
             .collect()
     }
-
 
     sealed class ViewState {
         data class Item(val data: MsgItemPresentation) : ViewState()
